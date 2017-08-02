96ee42ba3d66bdd2400ab5b3636ed41257e13a08
simonepri <simone.pri@hotmail.it>
2017-08-02 10:48:32 +0200

    Fix tests

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 import test from 'ava';
 import clearRequire from 'clear-require';
-import m from '.';
 
 test.beforeEach(() => {
   clearRequire('.');
 });
 
 test.serial('should return the version', t => {
+  const m = require('.');
+
   t.is(m, '1.0.0');
 });
 
 test.serial('should return the version if the env var is undefined', t => {
+  const m = require('.');
+
   const original = process.env.npm_package_version;
   process.env.npm_package_version = undefined; // eslint-disable-line camelcase
   t.is(m, '1.0.0');
